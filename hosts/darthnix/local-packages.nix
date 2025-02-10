826dc84d97ebf903933e749be03cfdc974f28aa7
ilosrim <ilosrim@yahoo.com>
2025-02-10 16:23:48 +0500

    chaged

--- hosts/darthnix/local-packages.nix
+++ hosts/darthnix/local-packages.nix
@@ -3,15 +3,12 @@
     # Desktop apps
     telegram-desktop
     chromium
-    pymol
     postman
-    gparted
 
     # Office
     libreoffice
 
     # CLI Apps
-    autodock-vina
 
     # WM stuff
     libsForQt5.xwaylandvideobridge
@@ -25,32 +22,5 @@
 
     # Others
     boost
-
-    # ADFR muhiti
-    (pkgs.buildFHSUserEnv {
-      name = "adfr-env";
-      targetPkgs = pkgs:
-        with pkgs; [
-          bash
-          # pip env
-          python311Packages.pip
-          python311Packages.vina
-          python311Packages.numpy
-          python311Packages.scipy
-          python311Packages.rdkit
-          python311Packages.meeko
-          python311Packages.gemmi
-
-          coreutils
-          glibc
-          xorg.libX11
-          xorg.libXext
-          xorg.libXrender
-          xorg.libXft
-          fontconfig
-        ];
-      profile = "export PATH=$PWD:\$PATH";
-      runScript = "bash";
-    })
   ];
 }
