e0b2169dc261b32d8b7b3bf635cb50c241c18978
ilosrim <ilosrim@yahoo.com>
2025-02-12 21:16:09 +0500

    boot config changed

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -1,12 +1,21 @@
 {
-  boot.loader = {
-    systemd-boot = {
-      enable = true;
-      extraEntries."ubuntu.conf" = ''
-        title Ubuntu 22.04
-        efi   /efi/ubuntu/shimx64.efi
-      '';
-    };
-    efi.canTouchEfiVariables = true;
-  };
+  # New Config
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.devices = ["nodev"];
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = true;
+
+  # Old config
+  # boot.loader = {
+  #   systemd-boot = {
+  #     enable = true;
+  #     extraEntries."ubuntu.conf" = ''
+  #       title Ubuntu 22.04
+  #       efi   /efi/ubuntu/shimx64.efi
+  #     '';
+  #   };
+  #   efi.canTouchEfiVariables = true;
+  # };
 }
