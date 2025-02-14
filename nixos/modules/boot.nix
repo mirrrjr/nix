afb5516651c29af576deffeec7eb827c8fc6205f
ilosrim <ilosrim@yahoo.com>
2025-02-14 20:32:49 +0500

    boot config changed

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -10,10 +10,10 @@
   #   efi.canTouchEfiVariables = true;
   # };
   #
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.systemd-boot.enable = false;
   boot.loader.grub.enable = true;
   boot.loader.grub.devices = ["nodev"];
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.useOSProber = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 }
