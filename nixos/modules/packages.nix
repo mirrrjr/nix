fe84a80c199bc61c7cdb963110948d9746dff967
mirrrrrrrrrrrr <mirrrrrr@yahoo.com>
2025-07-21 11:00:23 +0500

    fixed issues

--- nixos/modules/packages.nix
+++ nixos/modules/packages.nix
@@ -1,10 +1,10 @@
-{...}: {
+{pkgs, ...}: {
   nixpkgs.config = {
     allowUnfree = true;
     # permittedInsecurePackages = ["python-2.7.18.8" "electron-25.9.0"];
   };
 
-  environment.systemPackages = [
+  environment.systemPackages = with pkgs; [
     gcc
     boost
   ];
